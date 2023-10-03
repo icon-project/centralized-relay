80f527a2bb752a54165ee67227724d238aeac42b
viveksharmapoudel <viveksharmapoudel@gmail.com>
2023-10-03 17:44:07 +0545

    chore: add test cases

--- relayer/types/types_test.go
+++ relayer/types/types_test.go
@@ -6,6 +6,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestRouteMessage(t *testing.T) {
+
+	m1 := Message{
+		Dst: "mock-2",
+		Src: "mock-1",
+		Sn:  1,
+	}
+
+	routeMessage := NewRouteMessage(m1)
+
+	t.Run("route message set processing", func(t *testing.T) {
+		routeMessage.SetIsProcessing(true)
+		assert.Equal(t, true, routeMessage.GetIsProcessing())
+	})
+
+	t.Run("route message increment retry", func(t *testing.T) {
+		routeMessage.IncrementRetry()
+		assert.Equal(t, uint64(1), routeMessage.GetRetry())
+	})
+
+	t.Run("getMessage from routeMessage", func(t *testing.T) {
+		assert.Equal(t, m1, routeMessage.GetMessage())
+	})
+}
+
 func TestMessageCache(t *testing.T) {
 
 	messageCache := NewMessageCache()
