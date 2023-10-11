48de3a07da22bda1e6446906e0c8f7683abf54db
viveksharmapoudel <viveksharmapoudel@gmail.com>
2023-10-11 23:00:31 +0545

    feat: events

---
+++ relayer/chains/icon/events.go
@@ -0,0 +1,65 @@
+package icon
+
+import (
+	"encoding/hex"
+	"strings"
+
+	"github.com/icon-project/centralized-relay/relayer/chains/icon/types"
+)
+
+// Events
+var (
+	// All the events
+	EmitMessage = "EmitMessage(str,bytes)"
+)
+
+func MustConvertEventNameToBytes(eventName string) []byte {
+	return []byte(eventName)
+}
+
+func ToEventLogBytes(evt types.EventLogStr) types.EventLog {
+	indexed := make([][]byte, 0)
+
+	for _, idx := range evt.Indexed {
+		indexed = append(indexed, []byte(idx))
+	}
+
+	data := make([][]byte, 0)
+
+	for _, d := range evt.Data {
+		if isHexString(d) {
+			filtered, _ := hex.DecodeString(strings.TrimPrefix(d, "0x"))
+			data = append(data, filtered)
+			continue
+		}
+		data = append(data, []byte(d))
+	}
+
+	return types.EventLog{
+		Addr:    evt.Addr,
+		Indexed: indexed,
+		Data:    data,
+	}
+
+}
+
+var MonitorEvents []string = []string{
+	//TODO: list all the events to monitor
+	EmitMessage,
+}
+
+func GetMonitorEventFilters(address string) []*types.EventFilter {
+
+	filters := []*types.EventFilter{}
+	if address == "" {
+		return filters
+	}
+
+	for _, event := range MonitorEvents {
+		filters = append(filters, &types.EventFilter{
+			Addr:      types.Address(address),
+			Signature: event,
+		})
+	}
+	return filters
+}
