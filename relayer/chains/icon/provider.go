6a0b8b9dfb58bdcb66265ad2ce29a5cf5ada961f
viveksharmapoudel <viveksharmapoudel@gmail.com>
2023-10-11 19:46:44 +0545

    feat: using goloop client for icon provider

--- relayer/chains/icon/provider.go
+++ relayer/chains/icon/provider.go
@@ -2,8 +2,10 @@ package icon
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/icon-project/centralized-relay/relayer/provider"
+	goloopclient "github.com/icon-project/goloop/client"
 	"go.uber.org/zap"
 )
 
@@ -11,7 +13,10 @@ import (
  * The provider assumes the key is in
  */
 type IconProviderConfig struct {
-	ChainID string `json:"chain-id" yaml:"chain-id"`
+	ChainID  string `json:"chain-id" yaml:"chain-id"`
+	KeyStore string `json:"key-store" yaml:"key-store"`
+	RPCAddr  string `json:"rpc-addr" yaml:"rpc-addr"`
+	Password string `json:"password" yaml:"password"`
 }
 
 // NewProvider should provide a new Icon provider
@@ -20,19 +25,28 @@ func (pp *IconProviderConfig) NewProvider(log *zap.Logger, homepath string, debu
 	if err := pp.Validate(); err != nil {
 		return nil, err
 	}
+
 	return &IconProvider{
-		log: log.With(zap.String("chain_id", pp.ChainID)),
+		log:    log.With(zap.String("chain_id", pp.ChainID)),
+		client: goloopclient.NewClientV3(pp.RPCAddr),
 	}, nil
 
 }
 
 func (pp *IconProviderConfig) Validate() error {
+	if pp.RPCAddr == "" {
+		return fmt.Errorf("icon provider rpc endpoint is empty")
+	}
+
+	// check keystore and password also matches
+
 	return nil
 }
 
 type IconProvider struct {
-	log  *zap.Logger
-	PCfg *IconProviderConfig
+	log    *zap.Logger
+	PCfg   *IconProviderConfig
+	client *goloopclient.ClientV3
 }
 
 func (icp *IconProvider) ChainId() string {
