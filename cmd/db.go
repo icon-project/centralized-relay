a5ec7dad6998e1bc579355690723cee8b5fa97d9
Debendra Oli <debendraoli@pm.me>
2023-11-30 11:45:41 +0545

    feat: prune cmd

--- cmd/db.go
+++ cmd/db.go
@@ -2,17 +2,50 @@ package cmd
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 
+	"github.com/icon-project/centralized-relay/relayer/lvldb"
 	"github.com/spf13/cobra"
 )
 
 func dbCmd(a *appState) *cobra.Command {
-	db := &cobra.Command{
+	var db *lvldb.LVLDB
+	dbCMD := &cobra.Command{
 		Use:     "db",
 		Short:   "Manage the database",
 		Aliases: []string{"d"},
 		Example: strings.TrimSpace(fmt.Sprintf(`$ %s db [command]`, appName)),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var err error
+			db, err = lvldb.NewLvlDB(filepath.Join(defaultHome, defaultDBName))
+			if err != nil {
+				return err
+			}
+			return nil
+		},
 	}
-	return db
+
+	pruneCmd := &cobra.Command{
+		Use:   "prune",
+		Short: "Prune the database",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Pruning the database...")
+			db.ClearStore()
+		},
+	}
+
+	messagesCmd := &cobra.Command{
+		Use:   "messages",
+		Short: "Get messages stored in the database",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO:
+			fmt.Println("Getting messages stored in the database...")
+		},
+	}
+
+	dbCMD.AddCommand(pruneCmd)
+	dbCMD.AddCommand(messagesCmd)
+
+	return dbCMD
 }
