706523991b259dc6f6c5af3a40787d07731402cd
Debendra Oli <debendraoli@pm.me>
2024-02-05 11:42:33 +0545

    add(docs): Explain each config field

--- cmd/root.go
+++ cmd/root.go
@@ -19,7 +19,7 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-const appName = "crly"
+const appName = "centralized-relay"
 
 var (
 	defaultHome   = filepath.Join(os.Getenv("HOME"), ".centralized-relay")
