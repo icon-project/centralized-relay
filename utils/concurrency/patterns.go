f3ca797efa3a8ddaf9fc874f8a5af81bd6afc3ed
sherpalden <sherpalden369@gmail.com>
2024-04-16 16:14:35 +0545

    chore: proper variable name and comments

--- utils/concurrency/patterns.go
+++ utils/concurrency/patterns.go
@@ -2,6 +2,12 @@ package concurrency
 
 import "sync"
 
+/*
+FanIn multiplexes multiple input channels into a single output channel. It reads from each input channel
+concurrently until all channels are closed, then closes the output channel. The function takes a 'done'
+channel to signal cancellation, and variadic 'channels' representing the input channels to multiplex.
+It returns a single output channel where values from all input channels are sent.
+*/
 func FanIn(done <-chan interface{}, channels ...<-chan interface{}) <-chan interface{} {
 	multiplex := func(c <-chan interface{}, wg *sync.WaitGroup, mStream chan interface{}) {
 		defer wg.Done()
@@ -30,11 +36,16 @@ func FanIn(done <-chan interface{}, channels ...<-chan interface{}) <-chan inter
 	return multiplexedStream
 }
 
-func Take(done <-chan interface{}, valueStream <-chan interface{}, freq int) <-chan interface{} {
+/*
+Take reads values from the 'valueStream' input channel and sends them to the output channel 'takeStream'.
+It reads until 'noOfItemsToTake' items are taken from 'valueStream' or a cancellation signal is received
+on the 'done' channel. The function returns a channel 'takeStream' where the taken values are sent.
+*/
+func Take(done <-chan interface{}, valueStream <-chan interface{}, noOfItemsToTake int) <-chan interface{} {
 	takeStream := make(chan interface{})
 	go func() {
 		defer close(takeStream)
-		for i := 0; i < freq; i++ {
+		for i := 0; i < noOfItemsToTake; i++ {
 			select {
 			case <-done:
 				return
