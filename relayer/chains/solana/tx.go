a49379e5cdc43ab78b93c735f72798c853e6d85a
sherpalden <sherpalden369@gmail.com>
2024-05-15 11:36:31 +0545

    chore: define contract call params

--- relayer/chains/solana/tx.go
+++ relayer/chains/solana/tx.go
@@ -3,10 +3,21 @@ package solana
 import (
 	"context"
 
+	"github.com/gagliardetto/solana-go"
+	relayerevents "github.com/icon-project/centralized-relay/relayer/events"
 	relayertypes "github.com/icon-project/centralized-relay/relayer/types"
 )
 
+func (p *Provider) MakeCallInstructions(msg *relayertypes.Message) ([]solana.GenericInstruction, error) {
+	switch msg.EventType {
+	case relayerevents.EmitMessage:
+
+	}
+	return nil, nil
+}
+
 func (p *Provider) Route(ctx context.Context, message *relayertypes.Message, callback relayertypes.TxResponseFunc) error {
+
 	return nil
 }
 
