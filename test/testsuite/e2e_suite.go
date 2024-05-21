772cfe762553e826f7a8e56c64044000aa51fe00
Biru C. Sainju <biru.sainju@ibriz.ai>
2024-05-21 10:46:21 +0545

    feat: added e2e testing integration for stellar chain

--- test/testsuite/e2e_suite.go
+++ test/testsuite/e2e_suite.go
@@ -8,7 +8,6 @@ import (
 
 func (s *E2ETestSuite) SetupXCall(ctx context.Context) error {
 	createdChains := s.GetChains()
-	// chainA, chainB := createdChains[0], createdChains[1]
 	for index, chain := range createdChains {
 		if err := chain.SetupXCall(ctx); err != nil {
 			return err
@@ -21,20 +20,6 @@ func (s *E2ETestSuite) SetupXCall(ctx context.Context) error {
 			}
 		}
 	}
-	// if err := chainA.SetupXCall(ctx); err != nil {
-	// 	return err
-	// }
-
-	// if err := chainA.SetupConnection(ctx, chainB); err != nil {
-	// 	return err
-	// }
-
-	// if err := chainB.SetupXCall(ctx); err != nil {
-	// 	return err
-	// }
-	// if err := chainB.SetupConnection(ctx, chainA); err != nil {
-	// 	return err
-	// }
 	return nil
 }
 
@@ -46,18 +31,7 @@ func (s *E2ETestSuite) SetupChainsAndRelayer(ctx context.Context) ibc.Relayer {
 	relayer, err := s.SetupRelayer(ctx, "centralized")
 
 	s.Require().NoErrorf(err, "Error while configuring relayer %v", err)
-	//eRep := s.GetRelayerExecReporter()
 
-	//pathName := s.GeneratePathName()
-	//chainA, chainB := s.GetChains()
-
-	//s.Require().NoErrorf(relayer.GeneratePath(ctx, eRep, chainA.(ibc.Chain).Config().ChainID, chainB.(ibc.Chain).Config().ChainID, pathName), "Error on generating path, %v", err)
-	//err = relayer.CreateClients(ctx, eRep, pathName, ibc.CreateClientOptions{
-	//	TrustingPeriod: "100000m",
-	//})
-	//s.Require().NoErrorf(err, "Error while creating client relayer : %s, %v", pathName, err)
-
-	//s.Require().NoError(relayer.CreateConnections(ctx, eRep, pathName))
 	s.Require().NoError(s.StartRelayer(relayer))
 	return relayer
 }
