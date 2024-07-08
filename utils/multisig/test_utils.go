b168f431e3318a1120794cffd6bc9e5b7100a1d5
zuttodoS <noobv2ram@gmail.com>
2024-07-08 17:34:11 +0700

    remove redundant import

--- utils/multisig/test_utils.go
+++ utils/multisig/test_utils.go
@@ -8,9 +8,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 
-	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/gin-gonic/gin"
