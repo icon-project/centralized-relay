1367c441a62727e8c9e20de66b33717f05b0bf1c
homelander <heocon161106@gmail.com>
2024-08-09 15:37:41 +0700

    converted to xcall message

--- relayer/chains/bitcoin/helper.go
+++ relayer/chains/bitcoin/helper.go
@@ -1,50 +1,158 @@
 package bitcoin
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "strconv"
+	"encoding/json"
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+	"strconv"
+	"strings"
+
+	bitcoinABI "github.com/icon-project/centralized-relay/relayer/chains/bitcoin/abi"
+	"github.com/icon-project/centralized-relay/utils/multisig"
+	"github.com/icon-project/icon-bridge/common/codec"
 )
 
 func GetRuneTxIndex(endpoint, method, bearToken, txId string, index int) (*RuneTxIndexResponse, error) {
-    client := &http.Client{}
-    endpoint = endpoint + "/utxo/" + txId + "/" + strconv.FormatUint(uint64(index), 10) + "/balance"
-    fmt.Println(endpoint)
-    req, err := http.NewRequest(method, endpoint, nil)
+	client := &http.Client{}
+	endpoint = endpoint + "/utxo/" + txId + "/" + strconv.FormatUint(uint64(index), 10) + "/balance"
+	fmt.Println(endpoint)
+	req, err := http.NewRequest(method, endpoint, nil)
 
-    if err != nil {
-        fmt.Println(err)
-        return nil, err
-    }
-    req.Header.Add("Authorization", bearToken)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	req.Header.Add("Authorization", bearToken)
 
-    res, err := client.Do(req)
-    if err != nil {
-        fmt.Println(err)
-        return nil, err
-    }
-    defer res.Body.Close()
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
 
-    body, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        fmt.Println(err)
-        return nil, err
-    }
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
 
-    var resp *RuneTxIndexResponse
-    err = json.Unmarshal(body, &resp)
-    if err != nil {
-        fmt.Println(err)
-        return nil, err
-    }
+	var resp *RuneTxIndexResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
 
-    return resp, nil
+	return resp, nil
 }
 
-func ConvertBtcMessageToXCallMessage() ([]byte, error) {
-    // get message
+func ToXCallMessage(data interface{}, from, to string, sn uint, protocols []string, requester, token0, token1 common.Address) ([]byte, error) {
+	var res []byte
 
-    return nil, nil
+	//bitcoinStateAbi, _ := abi.JSON(strings.NewReader(bitcoinABI.BitcoinStateMetaData.ABI))
+	nonfungibleABI, _ := abi.JSON(strings.NewReader(bitcoinABI.InonfungibleTokenMetaData.ABI))
+	addressTy, _ := abi.NewType("address", "", nil)
+	bytes, _ := abi.NewType("bytes", "", nil)
+
+	arguments := abi.Arguments{
+		{
+			Type: addressTy,
+		},
+		{
+			Type: bytes,
+		},
+	}
+
+	amount0, _ := big.NewInt(0).SetString("18999999999999999977305673", 10)
+
+	switch data.(type) {
+	case multisig.RadFiProvideLiquidityMsg:
+		dataMint := data.(multisig.RadFiProvideLiquidityMsg)
+		mintParams := bitcoinABI.INonfungiblePositionManagerMintParams{
+			Token0:         token0,
+			Token1:         token1,
+			Fee:            big.NewInt(int64(dataMint.Fee) * 100),
+			TickLower:      big.NewInt(int64(dataMint.LowerTick)),
+			TickUpper:      big.NewInt(int64(dataMint.UpperTick)),
+			Amount0Desired: amount0,
+			Amount1Desired: big.NewInt(539580403982610478),
+			Recipient:      common.HexToAddress(to),
+			Deadline:       big.NewInt(1000000000),
+		}
+
+		mintParams.Amount0Min = mulDiv(mintParams.Amount0Desired, big.NewInt(int64(dataMint.Min0)), big.NewInt(1e4))
+		mintParams.Amount1Min = mulDiv(mintParams.Amount1Desired, big.NewInt(int64(dataMint.Min1)), big.NewInt(1e4))
+
+		// encode
+		// todo: for init pool
+		//initPoolCalldata, err := bitcoinStateAbi.Pack("initPool", mintParams, "btc", "rad", 1e0)
+		//if err != nil {
+		//	return nil, err
+		//}
+
+		provideLiquidity, err := nonfungibleABI.Pack("mint", mintParams)
+		if err != nil {
+			return nil, err
+		}
+
+		// encode with requester
+		provideLiquidity, err = arguments.Pack(requester, provideLiquidity)
+		if err != nil {
+			return nil, err
+		}
+
+		//from := "0x3.BTC/bc1qvqkshkdj67uwvlwschyq8wja6df4juhewkg5fg"
+
+		// encode to xcall format
+		res, err = XcallFormat(provideLiquidity, from, to, sn, protocols)
+		if err != nil {
+			return nil, err
+		}
+
+	case multisig.RadFiWithdrawLiquidityMsg:
+
+	default:
+		return nil, fmt.Errorf("not supported")
+	}
+	return res, nil
+}
+
+func XcallFormat(callData []byte, from, to string, sn uint, protocols []string) ([]byte, error) {
+	//
+	csV2 := CSMessageRequestV2{
+		From:        from,
+		To:          to,
+		Sn:          big.NewInt(int64(sn)).Bytes(),
+		MessageType: uint8(CALL_MESSAGE_TYPE),
+		Data:        callData,
+		Protocols:   protocols,
+	}
+
+	//
+	cvV2EncodeMsg, err := codec.RLP.MarshalToBytes(csV2)
+	if err != nil {
+		return nil, err
+	}
+
+	message := CSMessage{
+		MsgType: big.NewInt(int64(CS_REQUEST)).Bytes(),
+		Payload: cvV2EncodeMsg,
+	}
+
+	//
+	finalMessage, err := codec.RLP.MarshalToBytes(message)
+	if err != nil {
+		return nil, err
+	}
+
+	return finalMessage, nil
+}
+
+func mulDiv(a, nNumerator, nDenominator *big.Int) *big.Int {
+	return big.NewInt(0).Div(big.NewInt(0).Mul(a, nNumerator), nDenominator)
 }
--- relayer/chains/bitcoin/types.go
+++ relayer/chains/bitcoin/types.go
@@ -1,56 +1,75 @@
 package bitcoin
 
 import (
-    "github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcd/wire"
 )
 
-type Action int
+type MessageType int
 
 const (
-    AddLiquidity Action = iota
-    Swap
-    WithdrawLiquidity
-    CollectFee
-    IncreaseLiquidity
-    DecreaseLiquidity
+	CS_REQUEST MessageType = iota + 1
+	CS_RESPONSE
+	CS_RESULT
+)
+
+type CallMessageType int
+
+const (
+	CALL_MESSAGE_TYPE MessageType = iota
+	CALL_MESSAGE_ROLLBACK_TYPE
+	PERSISTENT_MESSAGE_TYPE
 )
 
 type TxSearchParam struct {
-    StartHeight, EndHeight uint64
-    BitcoinScript          []byte
-    OPReturnPrefix         int
+	StartHeight, EndHeight uint64
+	BitcoinScript          []byte
+	OPReturnPrefix         int
 }
 
 type TxSearchRes struct {
-    Tx     *wire.MsgTx
-    Height uint64
+	Tx     *wire.MsgTx
+	Height uint64
 }
 
 // HightRange is a struct to represent a range of heights
 type HeightRange struct {
-    Start uint64
-    End   uint64
+	Start uint64
+	End   uint64
 }
 
 type XCallMessage struct {
-    Action       string
-    TokenAddress string
-    From         string
-    To           string
-    Amount       []byte
-    Data         []byte
+	Action       string
+	TokenAddress string
+	From         string
+	To           string
+	Amount       []byte
+	Data         []byte
 }
 
 type RuneInfo struct {
-    Rune         string `json:"rune"`
-    RuneId       string `json:"runeId"`
-    SpaceRune    string `json:"spaceRune"`
-    Amount       string `json:"amount"`
-    Symbol       string `json:"symbol"`
-    Divisibility int    `json:"divisibility"`
+	Rune         string `json:"rune"`
+	RuneId       string `json:"runeId"`
+	SpaceRune    string `json:"spaceRune"`
+	Amount       string `json:"amount"`
+	Symbol       string `json:"symbol"`
+	Divisibility int    `json:"divisibility"`
 }
 
 type RuneTxIndexResponse struct {
-    Code int        `json:"code"`
-    Data []RuneInfo `json:"data"`
+	Code int        `json:"code"`
+	Data []RuneInfo `json:"data"`
+}
+
+type CSMessageRequestV2 struct {
+	From        string   `json:"from"`
+	To          string   `json:"to"`
+	Sn          []byte   `json:"sn"`
+	MessageType uint8    `json:"messageType"`
+	Data        []byte   `json:"data"`
+	Protocols   []string `json:"protocols"`
+}
+
+type CSMessage struct {
+	MsgType []byte `json:"msgType"`
+	Payload []byte `json:"payload"`
 }
