248d10cb916ebf4447f6e4b8b4f053cab5ed6f9d
Debendra Oli <debendraoli@pm.me>
2024-08-12 13:38:54 +0545

    add: more test cases for retry logic

--- utils/retry/retry_test.go
+++ utils/retry/retry_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"testing"
-	"time"
 
 	"go.uber.org/zap"
 )
@@ -75,19 +74,32 @@ func TestRetry(t *testing.T) {
 		t.Errorf("Expected 0 attempts, got: %d", attempts)
 	}
 
-	// Test case 5: Operation takes longer than MaxRPCRetryDelay
+	// Test case 5: Operation fails on the first attempt
 	ctx = context.Background()
 	attempts = 0
+	expectedErr = errors.New("operation failed on the first attempt")
 	operation = func() error {
 		attempts++
-		time.Sleep(MaxRPCRetryDelay + time.Second)
-		return errors.New("operation failed")
+		return expectedErr
 	}
 	err = Retry(ctx, logger, operation, nil)
-	if err != nil {
-		t.Errorf("Expected no error, got: %v", err)
+	if err != expectedErr {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
 	}
-	if attempts != 1 {
-		t.Errorf("Expected 1 attempt, got: %d", attempts)
+
+	// Test case 6: Operation fails on all attempts
+	ctx = context.Background()
+	attempts = 0
+	expectedErr = errors.New("operation failed on all attempts")
+	operation = func() error {
+		attempts++
+		return expectedErr
+	}
+	err = Retry(ctx, logger, operation, nil)
+	if err != expectedErr {
+		t.Errorf("Expected error: %v, got: %v", expectedErr, err)
+	}
+	if attempts != RPCMaxRetryAttempts {
+		t.Errorf("Expected %d attempts, got: %d", RPCMaxRetryAttempts, attempts)
 	}
 }
