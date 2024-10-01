882f1409ae0ee1220ea4ee466b85da9385f0bc83
Biru C. Sainju <biru.sainju@ibriz.ai>
2024-10-01 12:59:42 +0545

    chore: fixed stellar config for cluster mode

--- relayer/chains/steller/config.go
+++ relayer/chains/steller/config.go
@@ -14,6 +14,7 @@ import (
 )
 
 type Config struct {
+	provider.CommonConfig
 	ChainID           string                         `json:"chain-id" yaml:"chain-id"`
 	ChainName         string                         `json:"-t" yaml:"-"`
 	HorizonUrl        string                         `json:"horizon-url" yaml:"horizon-url"`
