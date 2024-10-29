d12d2e0cb2e4a23f2737b8833eaa48ae63369c43
Biru C. Sainju <biru.sainju@ibriz.ai>
2024-10-29 10:05:50 +0545

    feat: revert unintended changes

--- relayer/chains/sui/keys_test.go
+++ relayer/chains/sui/keys_test.go
@@ -2,10 +2,8 @@ package sui
 
 import (
 	"encoding/hex"
-	"fmt"
 	"testing"
 
-	"github.com/coming-chat/go-sui/v2/sui_types"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,11 +19,4 @@ func TestRestoreKey(t *testing.T) {
 	assert.Equal(t, expectedAddr, key.Address)
 	assert.Equal(t, expectedPrivKey, hex.EncodeToString(key.KeyPair.PrivateKey()[:32]))
 
-	sign, _ := key.SignSecureWithoutEncode([]byte("helloworld"), sui_types.DefaultIntent())
-	signs := sign.Ed25519SuiSignature.Signature
-	// ms, _ := sign.MarshalJSON()
-	fmt.Println(hex.EncodeToString(key.KeyPair.PublicKey()))
-	fmt.Println(key.Address)
-
-	fmt.Println("message", hex.EncodeToString(signs[:]))
 }
