626433de753d16fa21663f4f5f63f57b910366cc
spiderlipovitan <spiderlipovitan@gmail.com>
2024-11-12 16:49:27 +0700

    fix: set rune amount when withdraw

--- utils/multisig/constants.go
+++ utils/multisig/constants.go
@@ -1,9 +1,9 @@
 package multisig
 
-const(
-	MIN_SAT = 100
-	SHARED_RANDOM_HEX = "6b628bf19d167cbaaee09a3c7a43159a8bf1111eae0703a144fb4caad92de49c"
-	DUST_UTXO_AMOUNT int64 = 1000
-	
+const (
+	MIN_SAT                 = 100
+	SHARED_RANDOM_HEX       = "6b628bf19d167cbaaee09a3c7a43159a8bf1111eae0703a144fb4caad92de49c"
+	DUST_UTXO_AMOUNT  int64 = 546
+
 	PART_LIMIT = 76
 )
